e02dde9d37488495d20e22639235caddbc52f579
fazlulhoque <33782547+fazlulhoque@users.noreply.github.com>
2017-11-18 18:40:08 +0600

    Initial commit

---
+++ IMyService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+using System.Web.Script.Services;
+
+namespace SamiulService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IMyService" in both code and config file together.
+    [ServiceContract]
+    public interface IMyService
+    {
+       
+        [OperationContract]
+        [WebGet(BodyStyle = WebMessageBodyStyle.Bare, RequestFormat = WebMessageFormat.Xml,
+        ResponseFormat = WebMessageFormat.Xml, UriTemplate = "xml/{projectId}")]
+        string XMLData(string projectId);
+
+
+        [OperationContract]
+        [WebGet(BodyStyle = WebMessageBodyStyle.Bare, RequestFormat = WebMessageFormat.Json,
+        ResponseFormat = WebMessageFormat.Json, UriTemplate = "json/{projectId}")]
+        string JSONData(string projectId);
+       
+        
+    }
+}
---
+++ MyService.svc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SamiulService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MyService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select MyService.svc or MyService.svc.cs at the Solution Explorer and start debugging.
+    public class MyService : IMyService
+    {
+        #region IRestService Members
+
+        public string XMLData(string id)
+        {
+            return "Your requested product" + id;
+        }
+
+
+        public string JSONData(string id)
+        {
+            return "Your requested product" + id;
+        }
+
+        #endregion
+    }
+}
